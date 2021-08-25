872751c6b3fce5bf0ba61de3f3cf5db13da322bd
ygorhsilva <ygohenrique@gmail.com>
2021-08-25 20:24:42 -0300

    Update main.c

--- Compilador_1/main.c
+++ Compilador_1/main.c
@@ -14,27 +14,6 @@ Este código é de livre distribuição e uso.
 
 char look; /* O caracter lido "antecipadamente" (lookahead) */
 
-/* protótipos */
-void init();
-void nextChar();
-void error(char *fmt, ...);
-void fatal(char *fmt, ...);
-void expected(char *fmt, ...);
-void skipWhite();
-void match(char c);
-void getName(char *name);
-void getNum(char *num);
-void emit(char *fmt, ...);
-int isAddOp(char c);
-void ident();
-void factor();
-void multiply();
-void divide();
-void term();
-void add();
-void subtract();
-void expression();
-void assignment();
 
 /* PROGRAMA PRINCIPAL */
 int main()
